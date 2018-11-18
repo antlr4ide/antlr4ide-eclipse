17384353a50a9dad5054b06cd910d8acc7977827
Henrik Sorensen <henrik.sorensen@tmaxsoft.com>
2018-11-17 19:16:27 -0800

    Improved trace msgs. Use Map for setting error markers

--- src/org/github/antlr4ide/editor/AntlrScanner.java
+++ src/org/github/antlr4ide/editor/AntlrScanner.java
@@ -18,6 +18,7 @@ public class AntlrScanner implements ITokenScanner  {
 	private static final boolean DEBUG_VERBOSE = false;
 
 	protected boolean DEBUG;
+	private int setRangeCount;
 
 	protected IDocument document;
 	protected static Map<Integer, IToken> hilite ;
@@ -41,8 +42,13 @@ public class AntlrScanner implements ITokenScanner  {
 	}
 	
 	public AntlrScanner(ANTLRv4Editor editor, boolean debug) {
-		this.DEBUG=debug;
+		this.setRangeCount=-1;
 		this.editor=editor;
+		this.DEBUG=debug;
+		if (DEBUG) {
+			System.out.println(">>> AntlrScanner Editor>" +editor + "< debug>" + debug + "<");
+		}
+		
 	}
 
 	public boolean setDebug(boolean debug) {
@@ -53,7 +59,7 @@ public class AntlrScanner implements ITokenScanner  {
 	
 	/**
 	 * Configures the scanner's default return token. This is the token
-	 * which is returned when none of the rules fired and EOF has not been
+	 * which is returned when none of the ruleops fired and EOF has not been
 	 * reached.
 	 *
 	 * @param defaultReturnToken the default return token
@@ -74,21 +80,22 @@ public class AntlrScanner implements ITokenScanner  {
 	 */
 	@Override
 	public void setRange(IDocument document, int offset, int length) {
-
+		setRangeCount++;
 		
 		if (DEBUG) {
-			System.out.println(">>> AntlrScanner.setRange offset>" + offset + "< length>" + length + "<");
+			System.out.println(">>> AntlrScanner.setRange ["+setRangeCount+"] offset>" + offset + "< length>" + length + "<");
 		}
 		
-		if (DEBUG_VERBOSE) {
-			System.out.println(">>> AntlrScanner.setRange offset>" + offset + "< length>" + length + "<");
+		if (DEBUG_VERBOSE||DEBUG) {
+			//System.out.println(">>> AntlrScanner.setRange offset>" + offset + "< length>" + length + "<");
 			Exception ex = new Exception();
 			int i = 0;
 			for (StackTraceElement ste : ex.getStackTrace()) {
-				System.out.println(ste.toString());
+				if(i<20)
+				    System.out.println(ste.toString());
+				else if (i > 20 && ste.toString().startsWith("org.github")) // only print related
+					System.out.println(ste.toString());
 				i++;
-				if (i > 20)
-					break;
 			}
 		}
 		
@@ -97,15 +104,15 @@ public class AntlrScanner implements ITokenScanner  {
 		
 		this.document= document;
 
-		if(offset==0) {
+//		if(offset==0) {
 		// TODO: check if all tokens needs to be rescanned
 		antlrTokens=getAllAntlrTokens();
 		tokenIndex=getAntlrTokenIndex(offset, length);
-		}
-		else {  // prevent rescanning after marker is set
-			antlrTokens=new ArrayList<Token>();
-			tokenIndex=Integer.MAX_VALUE-1;
-		}
+//		}
+//		else {  // prevent rescanning after marker is set
+//			antlrTokens=new ArrayList<Token>();
+//			tokenIndex=Integer.MAX_VALUE-1;
+//		}
 		
 	}
 
@@ -158,13 +165,13 @@ public class AntlrScanner implements ITokenScanner  {
 	}
 
 	private boolean isRangeOK(Token token) {
-//		if(DEBUG) { 
-//			System.out.println(">>> AntlrScanner.isRangeOK >"+token.getStartIndex()
-//			+","+(token.getStartIndex()< (setRangeOffset+setRangeLength))
-//		     +","+(token.getStartIndex()>=(setRangeOffset))
-//		     );
-//		}
-//
+		if(DEBUG_VERBOSE) { 
+			System.out.println(">>> AntlrScanner.isRangeOK >"+token.getStartIndex()
+			+","+(token.getStartIndex()< (setRangeOffset+setRangeLength))
+		     +","+(token.getStartIndex()>=(setRangeOffset))
+		     );
+		}
+
 		
 		if(token.getStartIndex()< (setRangeOffset+setRangeLength)
 		&& token.getStartIndex()>=(setRangeOffset)
@@ -182,28 +189,47 @@ public class AntlrScanner implements ITokenScanner  {
 
 		
 		
-		try {
+		try { errorList.clear();
 			LexerHelper lexer=new LexerHelper(parserRules,lexerRules,errorList);
 		antlrTokens=(List<Token>) lexer.scanString(this.document.get());
+		// TODO: Remove markers if error fixed
 		if(errorList.size()>0) {
 			// TODO: check error positions are within setRange offset+length
-			// annotate resource with errors.
+//			// annotate resource with errors.
 			System.out.println("Syntax errors:" + errorList);
-			//errorlist format  line:position:token.getStartIndex():token.getStopIndex(): message
+//			//errorlist format  line:position:token.getStartIndex():token.getStopIndex(): message
 			String ss[]=errorList.get(0).split(":");
 			int line=Integer.parseInt(ss[0]);
 			int start=Integer.parseInt(ss[2]);
 			int end=Integer.parseInt(ss[3])+1; //token.getStopIndex() is inclusive. char_end is exclusive so add +1
-			
+			System.out.println("["+setRangeCount+"]>>>> Setting Marker START <<<<");
 			// Note setting the marker on the file, causes eclipse to invoke the scanner for the impacted line
 			IEditorInput input = this.editor.getEditorInput();
 			IFile file = ((IFileEditorInput) input).getFile();
+			
+			// check if marker already exists
+			int markerset=0;
+			IMarker fileMarkers [] = file.findMarkers(IMarker.PROBLEM, false, 1);
+			//IMarker implementation org.eclipse.core.internal.resources.Marker mm;
+			for(IMarker m: fileMarkers) {
+				if(m.getAttribute(IMarker.SEVERITY,-1)==IMarker.SEVERITY_ERROR
+				&& m.getAttribute(IMarker.LINE_NUMBER,-1)==line
+				&& m.getAttribute(IMarker.CHAR_START,-1)==start
+				&& m.getAttribute(IMarker.CHAR_END,-1)==end) {markerset++; break;}
+			}
+			if (markerset==0) {
+			String mrkAttr[]= {IMarker.SEVERITY,IMarker.LINE_NUMBER,IMarker.MESSAGE,IMarker.CHAR_START,IMarker.CHAR_END};
+			Object mrkValue[] = {IMarker.SEVERITY_ERROR,line,ss[4],start,end} ;
+
 			IMarker marker = file.createMarker(IMarker.PROBLEM);
-			marker.setAttribute(IMarker.SEVERITY   , IMarker.SEVERITY_ERROR);
-			marker.setAttribute(IMarker.LINE_NUMBER, line);
-			marker.setAttribute(IMarker.MESSAGE    , ss[4]);
-			marker.setAttribute(IMarker.CHAR_START , start);
-			marker.setAttribute(IMarker.CHAR_END   , end);
+			marker.setAttributes(mrkAttr, mrkValue);
+//			marker.setAttribute(IMarker.SEVERITY   , IMarker.SEVERITY_ERROR);
+//			marker.setAttribute(IMarker.LINE_NUMBER, line);
+//			marker.setAttribute(IMarker.MESSAGE    , ss[4]);
+//			marker.setAttribute(IMarker.CHAR_START , start);
+//			marker.setAttribute(IMarker.CHAR_END   , end);
+			}
+			System.out.println("["+setRangeCount+"]>>>> Setting Marker DONE  <<<<");
 		}
 			
 		}
