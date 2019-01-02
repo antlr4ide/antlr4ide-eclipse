9e31b933e7ab5804303f982c3cfeae73931d7d63
Henrik Sorensen <henrik.sorensen@tmaxsoft.com>
2019-01-02 08:56:56 -0800

    Move adding console to ConsoleManager in getConsole

--- src/org/github/antlr4ide/console/AntlrConsoleFactory.java
+++ src/org/github/antlr4ide/console/AntlrConsoleFactory.java
@@ -3,7 +3,6 @@ package org.github.antlr4ide.console;
 import org.eclipse.ui.console.ConsolePlugin;
 import org.eclipse.ui.console.IConsole;
 import org.eclipse.ui.console.IConsoleFactory;
-import org.eclipse.ui.console.IConsoleManager;
 import org.eclipse.ui.console.IOConsole;
 
 public class AntlrConsoleFactory implements IConsoleFactory {
@@ -13,41 +12,38 @@ public class AntlrConsoleFactory implements IConsoleFactory {
 	public void openConsole() {
 		System.out.println("AntlrConsoleFactory - openConsole ");
 		
-		// TODO Auto-generated method stub
-	    IConsoleManager consoleManager = ConsolePlugin.getDefault().getConsoleManager();
 	    // check if Antlr Console is already added:
 	    IOConsole antlrConsole = getConsole(); 
-	    if(antlrConsole==null) {
-	    	// add Antlr Console to list
-	    	antlrConsole = new IOConsole(ANTLR_CONSOLE, ConsolePlugin.getImageDescriptor(ANTLR_CONSOLE_IMAGE));
-	    	consoleManager.addConsoles( new IConsole[] { antlrConsole } );
-	    }
-	    consoleManager.showConsoleView( antlrConsole );
-	    
-//	    testConsole(antlrConsole);
-	    
+	    ConsolePlugin.getDefault().getConsoleManager().showConsoleView( antlrConsole );
 	}
 
-//	private void testConsole(IOConsole antlrConsole) {
-//		System.out.println("AntlrConsoleFactory - testConsole ");
-//		IOConsoleOutputStream out = antlrConsole.newOutputStream();
-//		try {
-//			out.write("TESTING\n");
-//			out.setColor(IANTLRv4ColorConstants.STMTrgb);
-//			out.write("TESTING - DIFFERENT COLOR\n");
-//			out.flush();
-//			out.close();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	}
-	
 	public IOConsole getConsole() {
 		System.out.println("AntlrConsoleFactory - getConsole ");
 	    for(IConsole console:ConsolePlugin.getDefault().getConsoleManager().getConsoles()) {
 	    	if(console.getName().equals(ANTLR_CONSOLE)) { return (IOConsole) console; }
 	    }
-	    return null;
+	    // If not found add Antlr Console to list
+    	IOConsole antlrConsole = new IOConsole(ANTLR_CONSOLE, ConsolePlugin.getImageDescriptor(ANTLR_CONSOLE_IMAGE));
+    	ConsolePlugin.getDefault().getConsoleManager().addConsoles( new IConsole[] { antlrConsole } );
+	    
+//	    showWelcome(antlrConsole);
+	    
+	    return antlrConsole;
 	}
+
+//	private void showWelcome(IOConsole antlrConsole) {
+//	System.out.println("AntlrConsoleFactory - testConsole ");
+//	IOConsoleOutputStream out = antlrConsole.newOutputStream();
+//	try {
+//		out.write("TESTING\n");
+//		out.setColor(IANTLRv4ColorConstants.STMTrgb);
+//		out.write("TESTING - DIFFERENT COLOR\n");
+//		out.flush();
+//		out.close();
+//	} catch (IOException e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//	}
+//}
+
 }
