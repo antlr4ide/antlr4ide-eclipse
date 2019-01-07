fc529cde858a9726d56db77c708b9d069b2141ba
Henrik Sorensen <henrik.sorensen@tmaxsoft.com>
2019-01-07 09:25:34 -0800

    Prepare builder to invoke launch

--- src/org/github/antlr4ide/builder/tool/AntlrToolJob.java
+++ src/org/github/antlr4ide/builder/tool/AntlrToolJob.java
@@ -1,7 +1,6 @@
 package org.github.antlr4ide.builder.tool;
 
 import java.io.IOException;
-
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
@@ -13,22 +12,30 @@ import org.github.antlr4ide.console.AntlrConsoleFactory;
 public class AntlrToolJob extends Job {
 	private IFile file;
 
-	      public AntlrToolJob(IFile file) {
-		         super("AntlrTool Job");
-		         this.setPriority(BUILD);
-		         this.file=file;
+      public AntlrToolJob(IFile file) {
+	         super("AntlrTool Job");
+	         this.setPriority(BUILD);
+	         this.file=file;
+	}
+	public IStatus run(IProgressMonitor monitor) {
+         System.out.println("This is a job ("+file.getName()+")");
+         // Send message to Antlr Tool Console
+         IOConsoleOutputStream toolout = AntlrConsoleFactory.getConsole().newOutputStream();
+         try {
+			toolout.write("TOOL - tbd - "+file.getName()+" ... \n");
+			// Create Launch for file
+//			List<String> command=new ArrayList<>();
+//			command.add("java");
+//			command.add("-jar antr-4.x.1-complete.jar Tool");
+//				
+//			ProcessBuilder toolProcess=new ProcessBuilder(command);
+//			Redirect r= Redirect.to(file);
+//			toolProcess.redirectOutput(destination)
+			toolout.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		public IStatus run(IProgressMonitor monitor) {
-	         System.out.println("This is a job ("+file.getName()+")");
-	         // Send message to Antlr Tool Console
-	         IOConsoleOutputStream toolout = AntlrConsoleFactory.getConsole().newOutputStream();
-	         try {
-				toolout.write("TOOL - "+file.getName()+" ... \n");
-				toolout.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	         return Status.OK_STATUS;
-	      }
-	   }
+         return Status.OK_STATUS;
+      }
+   }
