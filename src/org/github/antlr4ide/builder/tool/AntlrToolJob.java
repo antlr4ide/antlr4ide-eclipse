d5015b2bd2406e831292642431c6d539f12d5ccc
Henrik Sorensen <henrik.sorensen@tmaxsoft.com>
2019-01-10 23:45:37 -0800

    Use antlrjob process and tooloptions

--- src/org/github/antlr4ide/builder/tool/AntlrToolJob.java
+++ src/org/github/antlr4ide/builder/tool/AntlrToolJob.java
@@ -1,6 +1,10 @@
 package org.github.antlr4ide.builder.tool;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
@@ -11,26 +15,58 @@ import org.github.antlr4ide.console.AntlrConsoleFactory;
 
 public class AntlrToolJob extends Job {
 	private IFile file;
+	private File ioFile; // java.io.File
 
       public AntlrToolJob(IFile file) {
 	         super("AntlrTool Job");
 	         this.setPriority(BUILD);
 	         this.file=file;
+	         this.ioFile=null;
 	}
+      public AntlrToolJob(File ioFile) {
+	         super("AntlrTool Job");
+	         this.setPriority(BUILD);
+	         this.file=null;
+	         this.ioFile=ioFile;
+	}
+    
+    private String getFileName() {
+    	if(this.file!=null) return file.getName();
+    	return this.ioFile.getName();
+    }
+      
+    public ProcessBuilder getProcessBuilder() {
+		// Create Launch for file
+		List<String> command=new ArrayList<>();
+		command.add("java");
+		command.add("-jar");
+		command.add(AntlrToolConfig.getToolJar());
+		command.addAll(AntlrToolConfig.getToolOptions());
+//		command.add("name of grammar");
+			
+		ProcessBuilder toolProcess=new ProcessBuilder(command);
+//		Redirect r= Redirect.to(file);
+//		toolProcess.redirectOutput(destination)
+		
+		return toolProcess;
+    	
+    }
+    
 	public IStatus run(IProgressMonitor monitor) {
-         System.out.println("This is a job ("+file.getName()+")");
+         System.out.println("This is a job ("+getFileName()+")");
          // Send message to Antlr Tool Console
          IOConsoleOutputStream toolout = AntlrConsoleFactory.getConsole().newOutputStream();
          try {
-			toolout.write("TOOL - tbd - "+file.getName()+" ... \n");
-			// Create Launch for file
-//			List<String> command=new ArrayList<>();
-//			command.add("java");
-//			command.add("-jar antr-4.x.1-complete.jar Tool");
-//				
-//			ProcessBuilder toolProcess=new ProcessBuilder(command);
+			toolout.write("TOOL - tbd - "+getFileName()+" ... \n");
+			ProcessBuilder toolProcess=getProcessBuilder();
+			toolProcess.inheritIO();
+			
+			toolout.write(toolProcess.command().toString()+"\n");
+			
 //			Redirect r= Redirect.to(file);
 //			toolProcess.redirectOutput(destination)
+			toolProcess.start();
+
 			toolout.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
--- src/org/github/antlr4ide/builder/tool/AntlrToolLauncher.java
+++ src/org/github/antlr4ide/builder/tool/AntlrToolLauncher.java
@@ -1,126 +1,33 @@
 package org.github.antlr4ide.builder.tool;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
+import java.io.IOException;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
-import org.eclipse.debug.core.ILaunchConfigurationType;
-import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
-import org.eclipse.debug.core.ILaunchManager;
 import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
-import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
-import org.eclipse.jdt.launching.IRuntimeClasspathEntry;
-import org.eclipse.jdt.launching.IVMInstall;
-import org.eclipse.jdt.launching.JavaRuntime;
+import org.eclipse.debug.core.model.IProcess;
 
 
 public class AntlrToolLauncher implements ILaunchConfigurationDelegate {
 
-//    JarFile jf = new JarFile(jar);
-//
-//    List<URL> urls = new ArrayList<URL>(5);
-//    urls.add(jar.toURI().toURL());
-//    Manifest mf = jf.getManifest(); // if jar has a class-path in manfist add it's entries
-//    if (mf !=
-//            null) {
-//        String cp =
-//                mf.getMainAttributes().getValue("class-path");
-//        if (cp !=
-//                null) {
-//            for (String cpe : cp.split("\\s+")) {
-//                File lib =
-//                        new File(jar.getParentFile(), cpe);
-//                urls.add(lib.toURI().toURL());
-//            }
-//        }
-//    }
-//    ClassLoader cl =
-//            new URLClassLoader(urls.toArray(new URL[urls.size()]), classLoader);	
-	
-	private static final String nnaammee = "org.github.Antlr4.tool";  // must match plugin
-	protected IVMInstall jre;
-
-	public void StartAction(IVMInstall vm) {
-		//super(vm.getName());
-		jre = vm;
-	}
-
-	public void crxx() throws CoreException {
-	
-//	Creating a Launch Configuration
-//
-//	The first step is to create an instance of a launch configuration, used for launching local Java applications.
-//
-    ILaunchManager manager = DebugPlugin.getDefault().getLaunchManager();
-	ILaunchConfigurationType type =  manager.getLaunchConfigurationType(IJavaLaunchConfigurationConstants.ID_JAVA_APPLICATION);
-	ILaunchConfiguration[] configurations =  manager.getLaunchConfigurations(type);
-	
-	   for (int i = 0; i < configurations.length; i++) {
-	      ILaunchConfiguration configuration = configurations[i];
-	      if (configuration.getName().equals("Antlr4")) {
-	         configuration.delete();
-	         break;
-	      }
-	   }
-	ILaunchConfigurationWorkingCopy workingCopy = type.newInstance(null, "Antlr4");
-	// specify a JRE
-	workingCopy.setAttribute(IJavaLaunchConfigurationConstants.ATTR_VM_INSTALL_NAME, jre.getName());
-	workingCopy.setAttribute(IJavaLaunchConfigurationConstants.ATTR_VM_INSTALL_TYPE, jre.getVMInstallType().getId());
-	
-	// specify main type and program arguments
-	workingCopy.setAttribute(IJavaLaunchConfigurationConstants.ATTR_MAIN_TYPE_NAME, "org.apache.catalina.startup.Bootstrap");
-	workingCopy.setAttribute(IJavaLaunchConfigurationConstants.ATTR_PROGRAM_ARGUMENTS, "start");
-	
-	// specify classpath
-	File jdkHome = jre.getInstallLocation();
-	IPath toolsPath = new Path(jdkHome.getAbsolutePath()).append("lib").append("tools.jar");
-	IRuntimeClasspathEntry toolsEntry = JavaRuntime.newArchiveRuntimeClasspathEntry(toolsPath);
-	toolsEntry.setClasspathProperty(IRuntimeClasspathEntry.USER_CLASSES);
-	
-	IPath bootstrapPath = new Path("TOMCAT_HOME").append("bin").append("bootstrap.jar");
-	IRuntimeClasspathEntry bootstrapEntry = JavaRuntime.newVariableRuntimeClasspathEntry(bootstrapPath);
-	bootstrapEntry.setClasspathProperty(IRuntimeClasspathEntry.USER_CLASSES);
-	
-	IPath systemLibsPath = new Path(JavaRuntime.JRE_CONTAINER);
-	IRuntimeClasspathEntry systemLibsEntry = JavaRuntime.newRuntimeContainerClasspathEntry(systemLibsPath, IRuntimeClasspathEntry.STANDARD_CLASSES);
-	
-	List classpath = new ArrayList();
-	classpath.add(toolsEntry.getMemento());
-	classpath.add(bootstrapEntry.getMemento());
-	classpath.add(systemLibsEntry.getMemento());
-	
-	workingCopy.setAttribute(IJavaLaunchConfigurationConstants.ATTR_CLASSPATH, classpath);
-	workingCopy.setAttribute(IJavaLaunchConfigurationConstants.ATTR_DEFAULT_CLASSPATH, false);
-	
-	// specify System properties
-	workingCopy.setAttribute(IJavaLaunchConfigurationConstants.ATTR_VM_ARGUMENTS, "-Djava.endorsed.dirs=\"..\\common\\endorsed\" -Dcatalina.base=\"..\" -Dcatalina.home=\"..\" -Djava.io.tmpdir=\"..\\temp\"");
-	
-	// specify working diretory
-	File workingDir = JavaCore.getClasspathVariable("TOMCAT_HOME").append("bin").toFile();
-	workingCopy.setAttribute(IJavaLaunchConfigurationConstants.ATTR_WORKING_DIRECTORY, workingDir.getAbsolutePath());
-	
-	// save and launch
-	ILaunchConfiguration configuration = workingCopy.doSave();
-//	DebugUITools.launch(configuration, ILaunchManager.RUN_MODE);
-	
-	
-	}
-//
 	@Override
-	public void launch(ILaunchConfiguration arg0, String arg1, ILaunch arg2, IProgressMonitor arg3)
+	public void launch(ILaunchConfiguration config, String mode, ILaunch launch, IProgressMonitor progress)
 			throws CoreException {
 		//arg0.getWorkingCopy();
-		System.out.println("AntlrToolLauncher - launch - ILaunchConfiguration " +arg0.getAttributes());
+		System.out.println("AntlrToolLauncher - launch - ILaunchConfiguration " +config.getAttributes());
+		File file = new File (config.getAttribute(LaunchConstants.GRAMMAR, "")); 
+		
+		AntlrToolJob job=new AntlrToolJob(file);
+		progress.worked(10);
+		try {
+			IProcess process=DebugPlugin.newProcess(launch, job.getProcessBuilder().start(), "Antlr Tool");			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-
-	
 }
 
